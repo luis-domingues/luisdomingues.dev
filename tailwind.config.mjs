a6248b6b6200debe1c84227da566ce0bfb3427fd
luis-domingues <luisdomingues117@gmail.com>
2025-01-23 16:51:57 -0300

    feat: implements new extensions

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,18 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				nunito: ['Nunito', 'sans-serif']
+			},
+			backgroundColor: {
+				primary: '#090f1b',
+				secondary: '#2E3440'
+			},
+			colors: {
+				primary: '#f0f0f0'
+			}
+		},
 	},
 	plugins: [],
 }
