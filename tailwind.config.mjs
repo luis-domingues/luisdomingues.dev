8236edd380ea346c0cc8d198f78c4eda73d99bb8
luis-domingues <luisdomingues117@gmail.com>
2025-04-15 16:29:40 -0300

    refactor: remove unused theme extensions from Tailwind configuration

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,18 +2,7 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {
-			fontFamily: {
-				nunito: ['Nunito', 'sans-serif']
-			},
-			backgroundColor: {
-				primary: '#090f1b',
-				secondary: '#2E3440'
-			},
-			colors: {
-				primary: '#f0f0f0'
-			}
-		},
+		extend: {},
 	},
 	plugins: [],
 }
