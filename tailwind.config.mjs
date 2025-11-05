dedc93d555a8a91775beb1e1623ffddd4225ad64
Luís Paulo Domingues <luisdomingues117@gmail.com>
2025-11-05 13:57:03 -0300

    feat: add custom font families to tailwind cSS configuration

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,13 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				'jetbrains': ['JetBrains Mono', 'monospace'],
+				'rubik': ['Rubik', 'sans-serif'],
+				'sans': ['Rubik', 'sans-serif'],
+			},
+		},
 	},
 	plugins: [],
 }
